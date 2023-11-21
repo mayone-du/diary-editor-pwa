0b6776ac3d1ee4a60905c57a821707fc878cb245
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-11-21 19:22:28 +0900

    ✨ pwa plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({});
+
+module.exports = nextConfig;
