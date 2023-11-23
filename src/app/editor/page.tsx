7fd612914d32f25148d31814f35d0e834757a715
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-11-23 22:10:45 +0900

    🐛

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from "next";
 import { MarkdownEditor } from "./_components/markdown-editor";
 import { Box } from "@mantine/core";
+
+export const metadata: Metadata = {
+  title: "Diary Editor",
+  manifest: "/manifest.json",
+  robots: {
+    index: false,
+  },
+};
 
 const Page = async () => {
   return (
